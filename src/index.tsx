d0c8c504614828faff6901fb5dc59d8cea087f46
clynequ <clynequ@tencent.com>
2025-03-31 14:36:38 +0800

    readme

--- src/index.tsx
+++ src/index.tsx
@@ -169,26 +169,27 @@ export interface IRefMethod {
   textRef: HTMLDivElement | null,
 }
 interface ICommentInput {
-  cRef?: React.RefObject<IRefMethod>,
-  autoFocus?: boolean,
-  value?: string,
-  placeholder?: string,
-  customClassName?: string,
-  atDisable?: boolean,
-  submitDisable?: boolean,
-  submitLoading?: boolean,
-  submitText?: string,
-  showCancel?: boolean,
-  showSubmit?: boolean,
+  cRef?: React.RefObject<IRefMethod>, // 编辑器dom元素引用
+  autoFocus?: boolean, //  是否自动聚焦，默认false，
+  value?: string, //  初始化内容，默认空字符串，
+  placeholder?: string, //  占位符，默认请输入内容，shift + 回车支持换行，
+  customClassName?: string, //  自定义样式类名，
+  atDisable?: boolean, //  是否禁用@功能，默认false，
+  submitDisable?: boolean, //  是否禁用提交功能，默认false，
+  submitLoading?: boolean, //  是否提交按钮处于加载状态，默认false，
+  submitText?: string, //   提交按钮文案，默认发送，
+  showCancel?: boolean, //  是否展示取消按钮，默认false，
+  showSubmit?: boolean, //   是否展示提交按钮，默认true，
   extraCheckContent?: string | boolean, // 在提交栏左侧额外显示checkbox，为假时不展示
-  onSubmit: (value?: string) => void,
-  onCancel?: () => void,
-  onExtraCheckChange?: (checked: boolean) => void,
+  queryMembers: (params: { searchKey: string }) => Promise<IUser[]>, //   获取用户列表的接口，参考类型接口IUser
+  onSubmit?: (value?: string) => void, //    提交按钮点击事件，value为当前输入框内容，
+  onCancel?: () => void, //    取消按钮点击事件，
+  onExtraCheckChange?: (checked: boolean) => void, //   额外checkbox状态变化事件，checked为当前状态，
 }
 
 const CommentInput: React.FC<ICommentInput> = ({
   cRef,
-  autoFocus,
+  autoFocus = false,
   value = '',
   placeholder = '请输入内容，shift + 回车支持换行',
   customClassName,
@@ -200,6 +201,7 @@ const CommentInput: React.FC<ICommentInput> = ({
   extraCheckContent = false,
   showSubmit = true,
   onSubmit,
+  queryMembers,
   onCancel = () => null,
   onExtraCheckChange = () => null,
 }) => {
@@ -327,7 +329,7 @@ const CommentInput: React.FC<ICommentInput> = ({
   };
 
   const handleEditFinish = () => {
-    onSubmit(textRef.current?.innerHTML);
+    onSubmit?.(textRef.current?.innerHTML);
   };
 
   const handleChangePosition = debounce(() => {
@@ -495,6 +497,7 @@ const CommentInput: React.FC<ICommentInput> = ({
         content={(
           <UserSelectPopupContent
             searchKey={userInputKey}
+            queryMembers={queryMembers}
             onSelect={handleSelectUser}
             onHide={handleClosePopup}
           />
